008ab2fd50cb430c4d86813d2a1dc45901bf6c88
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2017-11-10 18:20:18 +0300

    Revert: ~~~tmp

--- compiler/testData/diagnostics/testsWithJsStdLib/native/optionlBody/native.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/native/optionlBody/native.kt
@@ -4,23 +4,6 @@
 external val baz: Int
 external val boo: Int = definedExternally
 
-external val x: dynamic
-external val x1: Int
-
-var y: Any? by <!PROPERTY_DELEGATION_BY_DYNAMIC!>x<!>
-var yd = x
-var y1: Any? by lazy { x1 }
-var y2 = x1
-
-fun fooo() {
-    x
-    x1
-}
-
-
-val a = baz
-val b by lazy { baz }
-
 external fun foo()
 external fun bar() { definedExternally }
 
@@ -55,24 +38,4 @@ external object O {
     fun bar(s: String): String = definedExternally
 }
 
-fun testRefs(t: T, c: C) {
-    baz
-    boo
-    foo()
-    bar()
-    t.baz
-    t.foo()
-    t.bar()
-    c.baz
-    c.boo
-    c.foo()
-    c.bar()
-    C.baz
-    C.boo
-    C.foo()
-    C.bar()
-    O.baz
-    O.boo
-    O.foo("")
-    O.bar("")
-}
+
