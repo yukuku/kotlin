c8e8add5e3c3110ee4987029132550dc04f0f9f9
Sergey Igushkin <hotkeytlt@gmail.com>
2017-12-05 18:32:46 +0300

    Add integration tests for Gralde build cache support. Add an up-to-date'ness test for subplugin options.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/BaseGradleIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/BaseGradleIT.kt
@@ -147,7 +147,8 @@ abstract class BaseGradleIT {
             val freeCommandLineArgs: List<String> = emptyList(),
             val kotlinVersion: String = KOTLIN_VERSION,
             val kotlinDaemonDebugPort: Int? = null,
-            val usePreciseJavaTracking: Boolean? = null
+            val usePreciseJavaTracking: Boolean? = null,
+            val withBuildCache: Boolean = false
     )
 
     open inner class Project(
@@ -453,6 +454,9 @@ abstract class BaseGradleIT {
                 }
                 System.getProperty("maven.repo.local")?.let {
                     add("-Dmaven.repo.local=$it") // TODO: proper escaping
+                }
+                if (options.withBuildCache) {
+                    add("--build-cache")
                 }
                 addAll(options.freeCommandLineArgs)
             }
