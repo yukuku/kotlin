6a024f1187eff5e580675db1434b8d96b75e3c2b
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-01-11 17:31:14 +0300

    Add more tests

--- compiler/testData/codegen/box/coroutines/crossInlineWithCapturedOuterReceiver.kt
+++ compiler/testData/codegen/box/coroutines/crossInlineWithCapturedOuterReceiver.kt
@@ -12,7 +12,7 @@ inline fun crossInlineBuilderConsumer(crossinline block: (String) -> Unit) = obj
     }
 }
 
-inline fun crossInlineBuilder(crossinline block: () -> Consumer) = block()
+inline fun inlineBuilder(block: () -> Consumer) = block()
 
 fun builder(c: suspend () -> Unit) {
     c.startCoroutine(EmptyContinuation)
@@ -43,7 +43,7 @@ class Container {
         }
     }
 
-    val consumer2 = crossInlineBuilder {
+    val consumer2 = inlineBuilder {
         object : Consumer {
             override fun consume(s: String) {
                 builder {
@@ -53,7 +53,7 @@ class Container {
         }
     }
 
-    val consumer3 = crossInlineBuilder {
+    val consumer3 = inlineBuilder {
         builderConsumer {
             object : Consumer {
                 override fun consume(s: String) {
@@ -61,6 +61,48 @@ class Container {
                 }
             }
         }
+    }
+
+    val consumer4 = crossInlineBuilderConsumer { s ->
+        object : Consumer {
+            override fun consume(s1: String) {
+                builder {
+                    y = s1
+                }
+            }
+        }
+    }
+
+    val consumer5 = crossInlineBuilderConsumer { s ->
+        builderConsumer {
+            object : Consumer {
+                override fun consume(s1: String) {
+                    y = s1
+                }
+            }
+        }
+    }
+
+    val consumer6 = crossInlineBuilderConsumer { s ->
+        val c = object : Consumer {
+            override fun consume(s1: String) {
+                builder {
+                    y = s1
+                }
+            }
+        }
+        c.consume(s)
+    }
+
+    val consumer7 = crossInlineBuilderConsumer { s ->
+        val c = builderConsumer {
+            object : Consumer {
+                override fun consume(s1: String) {
+                    y = s1
+                }
+            }
+        }
+        c.consume(s)
     }
 }
 
@@ -74,5 +116,16 @@ fun box(): String {
     c.y = "FAIL 3"
     c.consumer3.consume("OK")
     if (c.y != "OK") return c.y
-    return c.y
+    c.y = "OK"
+    c.consumer4.consume("FAIL 4")
+    if (c.y != "OK") return c.y
+    c.consumer5.consume("FAIL 5")
+    if (c.y != "OK") return c.y
+    c.y = "FAIL 6"
+    c.consumer6.consume("OK")
+    if (c.y != "OK") return c.y
+    c.y = "FAIL 7"
+    c.consumer7.consume("OK")
+    if (c.y != "OK") return c.y
+    return "OK"
 }
