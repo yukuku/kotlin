90a38c0303897b2479bb8e12167894cd1e435fd6
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-01-18 20:35:22 +0300

    -- Version

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/KotlinPluginUtil.java
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/KotlinPluginUtil.java
@@ -33,7 +33,8 @@ public class KotlinPluginUtil {
     }
 
     public static boolean isSnapshotVersion() {
-        return "@snapshot@".equals(getPluginVersion());
+        return false;
+        //return "@snapshot@".equals(getPluginVersion());
     }
 
     public static boolean isDevVersion() {
