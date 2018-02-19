7593bd0472a3c2e51a112eb98135fb053f4a5542
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-02-19 20:31:35 +0300

    Fix `KotlinType` of constructor call for inline classes

--- compiler/testData/codegen/box/inlineClasses/checkUnboxingResultFromTypeVariable.kt
+++ compiler/testData/codegen/box/inlineClasses/checkUnboxingResultFromTypeVariable.kt
@@ -8,16 +8,20 @@ fun box(): String {
     val asInt = Result<Int>(19)
     val asString = Result<String>("sample")
     val asResult = Result<Result<Int>>(asInt)
+    val asResultCtor = Result<Result<Int>>(Result<Int>(10))
 
     val unboxedInt = asInt.typed()
     val unboxedString = asString.typed()
     val unboxedResult = asResult.typed()
+    val unboxedAsCtor = asResultCtor.typed()
 
     if (unboxedInt != 19) return "fail"
     if (unboxedString != "sample") return "fail"
     if (unboxedResult.typed() != 19) return "fail"
+    if (unboxedAsCtor.typed() != 10) return "fail"
 
     if (asResult.typed().typed() != 19) return "fail"
+    if (asResultCtor.typed().typed() != 10) return "fail"
 
     return "OK"
 }
