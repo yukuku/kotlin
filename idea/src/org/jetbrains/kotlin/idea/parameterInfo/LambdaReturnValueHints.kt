deca6bd0666c3af076c1531d1381d6d9079ce132
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-02-20 15:42:02 +0300

    -- return lambda hint experiment

--- idea/src/org/jetbrains/kotlin/idea/parameterInfo/LambdaReturnValueHints.kt
+++ idea/src/org/jetbrains/kotlin/idea/parameterInfo/LambdaReturnValueHints.kt
@@ -9,7 +9,6 @@ import com.intellij.codeInsight.hints.InlayInfo
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
-import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.kotlin.resolve.bindingContextUtil.isUsedAsResultOfLambda
 
@@ -30,21 +29,7 @@ fun provideLambdaReturnValueHints(expression: KtExpression): List<InlayInfo> {
 
     val bindingContext = expression.analyze()
     if (expression.isUsedAsResultOfLambda(bindingContext)) {
-        val lambdaName = getNameOfFunctionThatTakesLambda(expression) ?: "lambda"
-        return listOf(InlayInfo("$TYPE_INFO_PREFIX^$lambdaName", expression.startOffset))
+        return listOf(InlayInfo("$TYPE_INFO_PREFIX^", expression.startOffset))
     }
     return emptyList()
-}
-
-private fun getNameOfFunctionThatTakesLambda(expression: KtExpression): String? {
-    val lambda = expression.getStrictParentOfType<KtLambdaExpression>() ?: return null
-    val callExpression = lambda.getStrictParentOfType<KtCallExpression>() ?: return null
-    if (callExpression.lambdaArguments.any { it.getLambdaExpression() == lambda }) {
-        val parent = lambda.parent
-        if (parent is KtLabeledExpression) {
-            return parent.getLabelName()
-        }
-        return (callExpression.calleeExpression as? KtNameReferenceExpression)?.getReferencedName()
-    }
-    return null
 }
